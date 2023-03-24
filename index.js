28652120118a35e388bd482719023e40999ced70
AhmadElBatanouni <egyptstyles1@gmail.com>
2023-03-24 13:25:50 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = new express();
+
+app.get('/students', (req, res) => {
+  const { temprature, humidity } = req.query;
+  console.log({ temprature, humidity });
+  res.send(
+    'You sent the following: temprature' + temprature + ', humidity:' + humidity
+  );
+});
+
+app.listen(process.env.PORT || 3001);
