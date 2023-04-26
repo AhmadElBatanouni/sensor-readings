f777673bf9d5207b7398761bf139d2029138b559
AhmadElBatanouni <90385722+AhmadElBatanouni@users.noreply.github.com>
2023-04-26 14:17:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ import express from 'express';
 
 const app = new express();
 
+app.use(express.json());
+
 app.get('/new-readings', (req, res) => {
   const { temprature, humidity } = req.query;
+  console.log(req.body);
   console.log({ temprature, humidity });
   res.send(
     'You sent the following: temprature' + temprature + ', humidity:' + humidity
